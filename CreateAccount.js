4cfbcd36f98db41ad0f4239ea0b07880ab63621a
Nick Teets <nickteets@gmail.com>
2018-01-02 14:44:05 -0700

    Add ability to post to employees table from native-app

--- CreateAccount.js
+++ CreateAccount.js
@@ -17,16 +17,34 @@ export default class CreateAccount extends Component<{}> {
     super()
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      position: '',
+      phone: '',
+      name: ''
     };
   }
 
-  createUser(email, password) {
+  createUser(email, password, position, phone, name) {
     firebase.auth().createUserWithEmailAndPassword(email,password)
       .then(response => this.props.setUser(response.email))
+      .then(this.postEmployee(email, position, phone, name))
       //catch error here
   }
 
+  postEmployee(email, position, phone, name) {
+    fetch(`http://localhost:4000/api/v1/employees`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          email, position, phone, name
+        })
+      }
+    )
+    .then(res => res.json())
+    .catch(error => { throw error; })
+  }
 
   render() {
     return (
@@ -34,6 +52,21 @@ export default class CreateAccount extends Component<{}> {
         <Text style={styles.login}>
           Create Account Here
         </Text>
+        <TextInput
+          style={{height:40}}
+          placeholder="name"
+          onChangeText={(text) => this.setState({name: text})}
+        />
+        <TextInput
+          style={{height:40}}
+          placeholder="position"
+          onChangeText={(text) => this.setState({position: text})}
+        />
+        <TextInput
+          style={{height:40}}
+          placeholder="phone"
+          onChangeText={(text) => this.setState({phone: text})}
+        />
         <TextInput
           style={{height:40}}
           placeholder="email"
@@ -45,7 +78,13 @@ export default class CreateAccount extends Component<{}> {
           onChangeText={(text) => this.setState({password: text})}
         />
         <Button
-          onPress={() => this.createUser(this.state.email, this.state.password)}
+          onPress={() => this.createUser(
+            this.state.email, 
+            this.state.password,
+            this.state.position,
+            this.state.phone,
+            this.state.name
+          )}
           title="Create Account"
         />
       </View>
